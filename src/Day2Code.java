69e3d0df34cb2714683ef98a3cbb925ce721513b
Chronos <randoduckman@duck.com>
2023-07-24 12:35:10 -0500

    I did some fixes about something something.

--- src/Day2Code.java
+++ src/Day2Code.java
@@ -1,5 +1,6 @@
 public class Day2Code {
     public static void main(String[] args) {
         System.out.println("Day2 code of my game, I created some avatars today");
+        System.out.println("I added more and more code to fix");
     }
 }
